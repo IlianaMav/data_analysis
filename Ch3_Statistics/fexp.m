6cee019b303b4b2dcd2fcbed27a6225b31a55059
IlianaMav <102994160+IlianaMav@users.noreply.github.com>
2023-02-11 18:46:03 +0200

    Create fexp.m

---
+++ Ch3_Statistics/fexp.m
@@ -0,0 +1,18 @@
+function y=fexp(tau,n,M,i)
+% fexp function generates M samples of size n from exponential distribution
+% with a given parameter lanmda, and calculate the sample mean for each of 
+% the M samples. Then it will make the histogram of the M sample means and 
+% give as output the average of the M sample means.
+
+x=exprnd(tau,n,M);
+meanx=mean(x);
+y=mean(meanx); 
+
+figure(i)
+clf
+histfit(meanx)
+title(['Distribution of sample mean (sample size: ', num2str(n), ')'])
+xlabel('Sample mean')
+ylabel('Counts')
+
+end

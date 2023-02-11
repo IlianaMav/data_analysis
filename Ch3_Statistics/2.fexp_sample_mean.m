2b7958fb691e648bc95ab606e742dcfc89f4b5ed
IlianaMav <102994160+IlianaMav@users.noreply.github.com>
2023-02-11 18:45:26 +0200

    Create 2.fexp_sample_mean.m

---
+++ Ch3_Statistics/2.fexp_sample_mean.m
@@ -0,0 +1,16 @@
+% Use of fexp function to generate M samples of size n and create the 
+% histogram of sample mean
+clear
+
+M=1000; %number of samples
+nmatrix=2.^(4:14); %size of each sample
+tau=1;
+%tau=5;
+imax=length(nmatrix);
+
+for i=1:imax
+n=nmatrix(i);
+fprintf('%d)sample size=%d\n',i,n)
+mean=fexp(tau,n,M,i);
+fprintf('mean=%.3f\n',mean)
+end

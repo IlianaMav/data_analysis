a4d46497cd04820851d2b5579549279b71eda648
IlianaMav <102994160+IlianaMav@users.noreply.github.com>
2023-02-11 20:10:45 +0200

    Update fpoisson.m

--- Ch3_Statistics/fpoisson.m
+++ Ch3_Statistics/fpoisson.m
@@ -1,6 +1,6 @@
 function y=fpoisson(lamda,n,M,i)
 % fpoisson function generates M samples of size n from Poisson distribution
-% with a given parameter lanmda, and calculate the sample mean for each of 
+% with a given parameter lamda, and calculate the sample mean for each of 
 % the M samples. Then it will make the histogram of the M sample means and 
 % give as output the average of the M sample means.
 
